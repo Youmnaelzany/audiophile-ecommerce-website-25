0301fa279c4aed4e6be81d271140abbda251be73
Youmna <youmnaprowork@gmail.com>
2025-01-15 23:06:32 +0200

    Update product links and adjust section margins in category components

--- src/components/EarphonesCategory.tsx
+++ src/components/EarphonesCategory.tsx
@@ -9,10 +9,10 @@ export default async function EarphonesCategory() {
   const productsZero = await getProductZero()
 
   return (
-    <section className="px-6 sm:pl-[2.44rem] sm:pr-10 lg:px-[10.31rem] space-y-[7.5rem] lg:space-y-40 mt-16 mb-28 sm:mt-[7.5rem] sm:mb-32 lg:my-40">
+    <section className="px-6 sm:pl-[2.44rem] sm:pr-10 lg:px-[10.31rem] space-y-[7.5rem] lg:space-y-40 mt-16 mb-[9.5rem]  sm:mt-[7.5rem] sm:mb-32 lg:my-40">
       {/* YX1 Wireless Earphones */}
       <ProductItem name={productsZero.name} description={productsZero.description} categoryImageDesk={productsZero.categoryImage.desktop}
-        newItem="NEW PRODUCT" link="/products/yx1-earphone"
+        newItem="NEW PRODUCT" link="/products/yx1-earphones"
         categoryImageTablet={productsZero.categoryImage.tablet} categoryImageMobile={productsZero.categoryImage.mobile} direction="right" />
     </section>
   );
--- src/components/SpeakersCategory.tsx
+++ src/components/SpeakersCategory.tsx
@@ -14,7 +14,7 @@ export default async function SpeakersCategory() {
   const productsFive = await getProductFive()
 
   return (
-    <section className="px-6 sm:pl-[2.44rem] sm:pr-10 lg:px-[10.31rem] space-y-[7.5rem] lg:space-y-40 mt-16 mb-28 sm:mt-[7.5rem] sm:mb-32 lg:my-40">
+    <section className="px-6 sm:pl-[2.44rem] sm:pr-10 lg:px-[10.31rem] space-y-[7.5rem] lg:space-y-40 mt-16 mb-[9.5rem] sm:mt-[7.5rem] sm:mb-32 lg:my-40">
       {/* ZX9 Speaker */}
       <ProductItem name={productsFive.name} description={productsFive.description} categoryImageDesk={productsFive.categoryImage.desktop}
         newItem="NEW PRODUCT" link="/products/zx9-speaker"
@@ -23,7 +23,7 @@ export default async function SpeakersCategory() {
       />
       {/* ZX7 Speaker */}
       <ProductItem name={productsFour.name} description={productsFour.description} categoryImageDesk={productsFour.categoryImage.desktop}
-        link="/products/zx9-speaker"
+        link="/products/zx7-speaker"
         categoryImageTablet={productsFour.categoryImage.tablet} categoryImageMobile={productsFour.categoryImage.mobile} direction="left" />
     </section>
   );
